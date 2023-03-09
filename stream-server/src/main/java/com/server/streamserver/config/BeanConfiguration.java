08b11926dbc355fe7da6f6a1d4fbfbb64976b446
FEN <fendrick85@gmail.com>
2023-03-10 06:51:35 +0700

    commit

---
+++ stream-server/src/main/java/com/server/streamserver/config/BeanConfiguration.java
@@ -0,0 +1,22 @@
+package com.server.streamserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public Executor asyncExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(1000);
+        executor.setThreadNamePrefix("MyAsync-");
+        executor.initialize();
+        return executor;
+    }
+}
